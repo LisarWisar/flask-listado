34ffb94ca108a6aef38c48aabcf7dd6e9d120b0c
LisarWisar <vsoto.zoro@gmail.com>
2025-04-24 11:48:12 -0400

    Modificaciones requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.12-slim
+RUN apt-get update && apt-get install -y build-essential gcc
 WORKDIR /app
 COPY . ./app
 COPY requirements.txt /tmp/
 RUN pip install --requirement /tmp/requirements.txt
 CMD ["python", "./src/app.py"]
-EXPOSE 3006
 
--- src/app.py
+++ src/app.py
@@ -1,31 +1,51 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from blueprints.tareas import tareas_blueprint
 from models import db
+import pymysql
 import os
+import time
 
 app = Flask(__name__)
 
-# Old SQlite DB
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# New MySQL DB
 user = os.getenv("MYSQL_USER", "root")
 password = os.getenv("MYSQL_PASSWORD:")
 host = os.getenv("MYSQL_HOST", "mysql")
 port = os.getenv("MYSQL_PORT", "3306")
 database = os.getenv("MYSQL_DB")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{user}:{password}@{host}:{port}/{database}"
+def espera_inicio(retries=5, delay=5):
+    user = os.getenv('MYSQL_USERNAME', "root")
+    password = os.getenv('MYSQL_PASSWORD')
+    host = os.getenv('MYSQL_HOST', "mysql")  
+    port = os.getenv('MYSQL_PORT')
+    database = os.getenv('MYSQL_DATABASE')
+    for i in range(retries):
+        try:
+            conn = pymysql.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database,
+                port=int(os.getenv('MYSQL_PORT', 3306)),
+                connect_timeout=5
+            )
+            conn.close()
+            return
+        except pymysql.MySQLError:
+            print(f"Esperando a que MySQL esté disponible... {i + 1}/{retries}")
+            time.sleep(delay)
+    raise RuntimeError("No se pudo conectar a MySQL después de varios intentos.")
+
+espera_inicio()
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.register_blueprint(tareas_blueprint, url_prefix="/tareas")
 db.init_app(app)
 migrate = Migrate(app, db)
 CORS(app)
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=True)
